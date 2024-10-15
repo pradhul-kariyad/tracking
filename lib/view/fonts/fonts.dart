e1091221ccaf17c8b3122941eaee233432e3eee9
unknown <pradhulkariyad@gmail.com>
2024-10-15 13:37:31 +0530

    47

--- lib/view/fonts/fonts.dart
+++ lib/view/fonts/fonts.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-final TextStyle pradhul = TextStyle(color: Colors.amber, fontSize: 12.sp);
+final TextStyle styleph1 = TextStyle(color: Colors.amber, fontSize: 12.sp);
+final TextStyle styleph2 = TextStyle(color: Colors.amber, fontSize: 12.sp);
